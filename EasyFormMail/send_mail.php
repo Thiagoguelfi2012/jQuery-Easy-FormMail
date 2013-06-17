76d92c5d8e8845711bb57c58e3a6f01e5f59f456
thiago <thiago@thiago-HP-1000-Notebook-PC>
2013-06-17 10:28:43 -0300

    jQuery Easy Formail Plugin - V1.1

--- EasyFormMail/send_mail.php
+++ EasyFormMail/send_mail.php
@@ -2,20 +2,19 @@
 // send_mail.php & jquery.EasyFormMail.js - See https://github.com/Thiagoguelfi2012/jQuery-Easy-FormMail for more info
 // Copyright (c) 2013 Thiago Valentoni Guelfi
 // Open Source - MIT License (MIT)
-
 if (PATH_SEPARATOR == ":") {
     $quebra = "\r\n";
 } else {
     $quebra = "\n";
 }
-if (isset($_REQUEST['to']) && !empty($_REQUEST['to'])) {
-    if (!isset($_REQUEST['subject']) || $_REQUEST['subject'] == NULL)
-        $_REQUEST['subject'] = "jQuery Easy FormMail";
-    if (!isset($_REQUEST['sucess_msg']) || $_REQUEST['sucess_msg'] == NULL)
-        $_REQUEST['sucess_msg'] = "Email sent successfully!";
+if (isset($_POST['to']) && !empty($_POST['to'])) {
+    if (!isset($_POST['subject']) || $_POST['subject'] == NULL)
+        $_POST['subject'] = "jQuery Easy FormMail";
+    if (!isset($_POST['sucess_msg']) || $_POST['sucess_msg'] == NULL)
+        $_POST['sucess_msg'] = "Email sent successfully!";
 
     $msg = "";
-    foreach ($_REQUEST as $key => $value) {
+    foreach ($_POST as $key => $value) {
         if ($key != "subject" && $key != "to" && $key != "sucess_msg" && $key != "error_msg"
                 && $key != "emptyField_msg" && $key != "_utmz" && $key != "_utma") {
             $msg .= ucwords($key) . ": " . $value . $quebra;
@@ -25,15 +24,15 @@ if (isset($_REQUEST['to']) && !empty($_REQUEST['to'])) {
 
     $headers = "MIME-Version: 1.1" . $quebra;
     $headers .= "Content-type: text/plain; charset=iso-8859-1" . $quebra;
-    $headers .= "From: " . $_REQUEST['subject'] . $quebra; //E-mail do remetente
-    $headers .= "Return-Path: " . $_REQUEST['subject'] . $quebra; //E-mail do remetente
-    if (mail($_REQUEST['to'], $_REQUEST['subject'], $msg, $headers, "-r" . $_REQUEST['to'])) {
-        ?><script type="text/javascript">alert("<?php echo $_REQUEST['sucess_msg']; ?>")</script><?php
+    $headers .= "From: " . $_POST['subject'] . $quebra; //E-mail do remetente
+    $headers .= "Return-Path: " . $_POST['subject'] . $quebra; //E-mail do remetente
+    if (mail($_POST['to'], $_POST['subject'], $msg, $headers, "-r" . $_POST['to'])) {
+        ?><script type="text/javascript">alert("<?php echo $_POST['sucess_msg']; ?>")</script><?php
     } else {
-        if (!isset($_REQUEST['error_msg']) || $_REQUEST['error_msg'] == NULL) {
+        if (!isset($_POST['error_msg']) || $_POST['error_msg'] == NULL) {
             ?><script type="text/javascript">alert(" Could not send the email!\n\ Make sure PHP is installed correctly and\n\ the mail configurations are enabled.")</script><?php
         } else {
-            ?><script type="text/javascript">alert("<?php echo $_REQUEST['error_msg']; ?>")</script><?php
+            ?><script type="text/javascript">alert("<?php echo $_POST['error_msg']; ?>")</script><?php
         }
     }
 } else {
